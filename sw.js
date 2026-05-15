b4d79e8e8ed48502fed88e88c0952d8901e8b80c
しあん <kassymokku@gmail.com>
2026-05-15 19:15:48 +0900

    Fix icon (correct PNG decode); bump SW cache to force refresh

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'nomi-v1';
+const CACHE = 'nomi-v2';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
